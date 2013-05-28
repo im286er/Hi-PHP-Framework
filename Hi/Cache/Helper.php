1dcbb4e0037f3368d6c9335b5339cb4f64d04d1b
weird <web4.0@qq.com>
2013-05-28 11:00:32 +0800

    Update Helper.php

--- Hi/Cache/Helper.php
+++ Hi/Cache/Helper.php
@@ -22,6 +22,12 @@ class Hi_Cache_Helper {
     private static $_cache_entity = array();
 
     /**
+	* 缓存实例池
+	* @var array
+	*/
+	private static $_instance = array();
+
+    /**
      *
      * @param string $instance
      * @return Lib_Cache_Abstract
@@ -64,7 +70,28 @@ class Hi_Cache_Helper {
         $c = self::get_instance($instance);
         $c->del($k);
     }
-
+	
+	/**
+     * 获取缓存实例对象
+     * @param string $provider 实例名
+     * @return Hi_Cache_Driver_Interface
+     */
+	public static function instance($provider=''){
+		if(!isset(self::$_instance[$provider])) {
+			//获取配置
+			if (!isset(Hi::$config->Cache[$provider])) {
+				 trigger_error("'{$provider}'缓存配置信息不存在",E_USER_ERROR);
+			}
+			$conf =  Hi::$config->Cache[$provider];
+			$driver = ucfirst(isset($conf['driver']) ? $conf['driver'] : 'file');
+            $classname = 'Hi_Cache_Driver_'.$driver;
+			if (!class_exists($this->_driver)) {
+				trigger_error("数据访问配置信息错误:'{$provider}'缓存访问所需驱动'{$this->_driver}'不存在",E_USER_ERROR);
+			}
+			$instance = new $classname($conf);
+			self::$_instance[$provider] = $instance;
+		}
+		return self::$_instance[$provider];
+	}
 }
-
 ?>
